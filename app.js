77c81770ca2311ee835ccac14c25efddfab7b635
Heorhi <vgerk95@gmail.com>
2020-05-20 16:43:31 +0300

    Add express lib

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const httpModule = require('http');
-const routes = require('./routes');
+const app = express();
 
-const server = httpModule.createServer(routes.requestHandler);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000, 'localhost');
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from home page</h1>');
+});
+
+app.listen(3000);
