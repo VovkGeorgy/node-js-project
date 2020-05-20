541061bbae78bc556c8a672518c6cfdee8d5aef8
Heorhi <vgerk95@gmail.com>
2020-05-20 17:29:45 +0300

    Add routing

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from home page</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found =(</h1>')
 });
 
 app.listen(3000);
