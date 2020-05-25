a70a3fd9a679480d3bc12450332d01da023df469
Heorhi <vgerk95@gmail.com>
2020-05-25 14:06:38 +0300

    Add pug render support

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin').routes;
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {pageTitle: '404 Page not found'});
 });
 
 app.listen(3000);
