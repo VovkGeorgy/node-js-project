6642459868ff9fe1b000fd7aaef2803e90a82f38
Heorhi <vgerk95@gmail.com>
2020-05-27 10:14:31 +0300

    Update structure, add views, rebuild controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,24 +1,30 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
     })
 }
 
 exports.postAddProduct = (req, res, next) => {
-    const prod = new Product(req.body.title);
+    const formData = req.body;
+    const prod = new Product(
+        formData.title,
+        formData.imageUrl,
+        formData.description,
+        formData.price
+    );
     prod.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products,
-            path: '/',
-            pageTitle: 'Shop',
+            pageTitle: 'Adminll products',
+            path: '/admin/products',
         });
     });
 }
